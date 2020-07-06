6d43a617885d25351f3b1b275dcde56b268169d9
hbeni <beni@hallinger.org>
2020-07-07 00:21:06 +0200

    Load test accepts bot sleep now

--- server/test/loadTest.sh
+++ server/test/loadTest.sh
@@ -3,15 +3,18 @@
 # Spawns fake pilots. Hordes of them!
 
 spacing=0.25
+botsleep=30
 
 if [[ -z "$1" ]]; then
 	echo "Usage $0 <numberOfBots> <spacingInSeconds>" 
 fi
 if [[ -n $2 ]]; then spacing=$2; fi
+if [[ -n $3 ]]; then botsleep=$3; fi
+
 
 function spawnBot {
-    echo "  CMD=test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 >$5 2>$5 &"
-    lua test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 >$5 2>$5 &
+    echo "  CMD=test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 --sleep=$6 >$5 2>$5 &"
+    lua test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 --sleep=$6 >$5 2>$5 &
 }
 
 
@@ -26,7 +29,7 @@ for i in $(seq 1 $1); do
 	# and his own sample file
 	cp recordings/fgcom.rec.testsample.fgcs /tmp/fgcom.rec.testsample-$i.fgcs
 
-	spawnBot $i /tmp/fgcom-bot-$i.pem /tmp/fgcom-bot-$i.key /tmp/fgcom.rec.testsample-$i.fgcs /tmp/fgcom-bot-$i.log
+	spawnBot $i /tmp/fgcom-bot-$i.pem /tmp/fgcom-bot-$i.key /tmp/fgcom.rec.testsample-$i.fgcs /tmp/fgcom-bot-$i.log $botsleep
 
 	sleep $spacing
 done
