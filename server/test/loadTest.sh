a80639eed25b96437a7c71e73b17f15a768bfebd
hbeni <beni@hallinger.org>
2020-07-07 08:35:56 +0200

    Load tester now can accept host parameter

--- server/test/loadTest.sh
+++ server/test/loadTest.sh
@@ -4,22 +4,29 @@
 
 spacing=0.25
 botsleep=30
+host="localhost"
+startat=1
+numBots=1
 
 if [[ -z "$1" ]]; then
-	echo "Usage $0 <numberOfBots> <spacingInSeconds>" 
+	echo "Usage $0 <startAtNr> <numberOfBots> <spacingInSeconds> <checkInterval> <host>"
 fi
-if [[ -n $2 ]]; then spacing=$2; fi
-if [[ -n $3 ]]; then botsleep=$3; fi
+if [[ -n $1 ]]; then startat=$1; fi
+if [[ -n $2 ]]; then numBots=$2; fi
+if [[ -n $3 ]]; then spacing=$3; fi
+if [[ -n $4 ]]; then botsleep=$4; fi
+if [[ -n $5 ]]; then host=$5; fi
 
 
 function spawnBot {
-    echo "  CMD=test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 --sleep=$6 >$5 2>$5 &"
-    lua test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 --sleep=$6 >$5 2>$5 &
+    bcmd="lua test/fgcom-fakepilot.bot.lua --id=$1 --cert=$2 --key=$3 --sample=$4 --sleep=$6 --host=$7"
+    echo "  cmd=$bcmd >$5 2>$5 &"
+    $cmd >$5 2>$5 &
 }
 
 
-for i in $(seq 1 $1); do
-	echo "spawning: $i"
+for i in $(seq $startat $(expr $startat + $numBots - 1)); do
+	echo "spawning bot: $i"
 
 	# give every bot his own cert/key
 	openssl genrsa -out /tmp/fgcom-bot-$i.key 2048 2> /dev/null
@@ -29,7 +36,7 @@ for i in $(seq 1 $1); do
 	# and his own sample file
 	cp recordings/fgcom.rec.testsample.fgcs /tmp/fgcom.rec.testsample-$i.fgcs
 
-	spawnBot $i /tmp/fgcom-bot-$i.pem /tmp/fgcom-bot-$i.key /tmp/fgcom.rec.testsample-$i.fgcs /tmp/fgcom-bot-$i.log $botsleep
+	spawnBot $i /tmp/fgcom-bot-$i.pem /tmp/fgcom-bot-$i.key /tmp/fgcom.rec.testsample-$i.fgcs /tmp/fgcom-bot-$i.log $botsleep $host
 
 	sleep $spacing
 done
