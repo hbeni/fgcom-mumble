41a79736105dbb73e40b1af002095fb9d9a1a432
hbeni <beni@hallinger.org>
2020-09-29 16:27:39 +0200

    Added openssl license notice

--- client/mumble-plugin/lib/updater.cpp
+++ client/mumble-plugin/lib/updater.cpp
@@ -14,6 +14,23 @@
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
+ * OpenSSL exception (under GNU GPL version 3 section 7):
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ * 
+ * This product includes software developed by the OpenSSL Project
+ * for use in the OpenSSL Toolkit (http://www.openssl.org/)
+ *
  * Mumble API:
  * Copyright 2019-2020 The Mumble Developers. All rights reserved.
  * Use of this source code is governed by a BSD-style license
@@ -31,6 +48,7 @@
 #include "fgcom-mumble.h"   // plugin version number
 
 // HTTPLib from https://github.com/yhirose/cpp-httplib (MIT-License)
+// This needs OpenSSL.
 #define CPPHTTPLIB_OPENSSL_SUPPORT 1
 #include "http/httplib.h"
 using namespace httplib;
