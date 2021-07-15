c3c72c155d32535be6000d60e1b3b69f4047e751
hbeni <beni@hallinger.org>
2021-07-15 11:09:08 +0200

    Server: basic pluginmsg testbots parse cmdline args

--- server/test/pluginSendtest-Rcsv.lua
+++ server/test/pluginSendtest-Rcsv.lua
@@ -2,8 +2,36 @@ mumble = require("mumble")  -- get the mumble API
 
 local host = "localhost"
 local port = 64738
+local channel = "test"
 local cert = "bot.pem"
 local key  = "bot.key"
+
+-- Parse cmdline args
+if arg[1] then
+    if arg[1]=="-h" or arg[1]=="--help" then
+        print(botname)
+        print("usage: "..arg[0].." [opt=val ...]")
+        print("  Options:")
+        print("    --host=    host to connect to           (default="..host..")")
+        print("    --port=    port to connect to           (default="..port..")")
+        print("    --channel= channel to join                  (default="..fgcom.channel..")")
+        print("    --cert=    path to PEM encoded cert     (default="..cert..")")
+        print("    --key=     path to the certs key        (default="..key..")")
+        os.exit(0)
+    end
+    
+    for _, opt in ipairs(arg) do
+        _, _, k, v = string.find(opt, "--(%w+)=(.+)")
+        if k=="host"    then host=v end
+        if k=="port"    then port=v end
+        if k=="channel"   then fgcom.channel=v end
+        if k=="cert"    then cert=v end
+        if k=="key"     then key=v end
+    end
+    
+end
+
+
 
 print("connecting to "..host.." on port "..port.." (cert: "..cert.."; key: "..key..")")
 local client = assert(mumble.connect(host, port, cert, key))
@@ -15,9 +43,12 @@ client:hook("OnServerSync", function(client, event)
     print("Sync done; server greeted with: ", event.welcome_text)
            
     -- try to join fgcom-mumble channel
-    local ch = client:getChannel("fgcom-mumble")
+    local ch = client:getChannel(channel)
     event.user:move(ch)
-    print("joined channel fgcom-mumble")
+    print("joined channel "..channel)
+
+    -- Ask present clients for data
+    client:sendPluginData("FGCOM:ICANHAZDATAPLZ", "orly!", ch:getUsers())
 end)
     
     
